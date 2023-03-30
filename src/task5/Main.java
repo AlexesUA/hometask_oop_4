60fb73e041d2eb275b7ca5371dde8a68f13c483f
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-30 20:23:20 +0300

    Create Main class

---
+++ src/task5/Main.java
@@ -0,0 +1,15 @@
+package task5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scInput = new Scanner(System.in);
+
+        System.out.print("Введіть температуру в градусах по Цельсію: ");
+        double celsius = scInput.nextDouble();
+
+        System.out.printf("%1$.2f градусів за цельсієм це %2$.2f градусів за Кельвіном, або %3$.2f градусів за Фаренгейтом. %n",
+                celsius, ConverterTemperature.convertToKelvin(celsius), ConverterTemperature.convertToFahrenheit(celsius));
+    }
+}
