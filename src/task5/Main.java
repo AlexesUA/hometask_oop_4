a30ef4b8b214498d26342a760d51987f45e8b5c6
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-30 20:26:13 +0300

    Finish task

--- src/task5/Main.java
+++ src/task5/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
         System.out.print("Введіть температуру в градусах по Цельсію: ");
         double celsius = scInput.nextDouble();
-
         System.out.printf("%1$.2f градусів за цельсієм це %2$.2f градусів за Кельвіном, або %3$.2f градусів за Фаренгейтом. %n",
                 celsius, ConverterTemperature.convertToKelvin(celsius), ConverterTemperature.convertToFahrenheit(celsius));
     }
